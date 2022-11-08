51d653db5bf50c04e8b1900e0b5420812f7bd081
Yauheni Tsurankou <Yauheni_Tsurankou@epam.com>
2022-11-08 17:57:30 +0000

    init: init project

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'no-param-reassign': 'off',
+  },
+};
